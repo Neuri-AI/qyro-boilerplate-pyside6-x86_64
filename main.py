8482d01db9c49912b67ac4691d3edf864bd7d4ad
runesc <fredogodofredodospuntosv@gmail.com>
2026-09-18 08:44:33 -0600

    Refactor main.py to integrate ApplicationContext and Component, update render method for improved UI display

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 import sys
-from ppg_runtime.application_context.${python_bindings} import ApplicationContext
-from ppg_runtime.application_context import PPGLifeCycle, init_lifecycle
-from ppg_runtime.application_context.devtools.reloader import hot_reloading
-from ppg_runtime.application_context.utils import app_is_frozen
 from ${python_bindings}.QtWidgets import QMainWindow, QLabel
+from qyro_engine import ApplicationContext
+from qyro_engine.ui.component import Component
 
-@init_lifecycle
-@hot_reloading
-class ${app_name}(QMainWindow, PPGLifeCycle):
 
-    def render_(self):
-       QLabel('Hello World!', parent=self)
+class ${app_name}(QMainWindow, Component, ApplicationContext):
 
-    def responsive_UI(self):
+    def component_will_mount(self):
         self.setMinimumSize(640, 480)
 
+    def render(self):
 
-if __name__ == '__main__':
-    appctxt = ApplicationContext()
+        label = QLabel(
+            f"Hello, World!\n\n\n"
+            f"App Title: {self.window_title}\n\n"
+            f"Active Icon: {self.app_icon}\n\n"
+            f"Platform: {self.platform.value} (Frozen: {self.is_frozen})\n",
+            parent=self
+        )
+        label.move(50, 50)
+        label.resize(label.sizeHint())
+
+
+
+if __name__ == "__main__":
     window = ${app_name}()
-    if not app_is_frozen():
-        window._init_hot_reload_system(__file__)
     window.show()
-    exec_func = getattr(appctxt.app, 'exec', appctxt.app.exec_)
-    sys.exit(exec_func())
+    sys.exit(window.exec())
